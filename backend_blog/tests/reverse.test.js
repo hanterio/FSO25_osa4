c0bcf4c170632ff29334275f9dc12e62803594dd
Hannes Vieth <hannesvieth@gmail.com>
2025-03-24 09:34:02 +0200

    Tähdellä merkatut tehtävät 4.5 ja 4.7 tehty

--- backend_blog/tests/reverse.test.js
+++ backend_blog/tests/reverse.test.js
@@ -8,45 +8,97 @@ test('dummy returns one', () => {
   const result = listHelper.dummy(blogs)
   assert.strictEqual(result, 1)
 })
-
-describe('total likes', () => {
-    const emptyList = []
-    const listWithOneBlog = [
-      {
+const emptyList = []
+const listWithOneBlog = [
+  {
+    _id: '5a422aa71b54a676234d17f8',
+    title: 'Go To Statement Considered Harmful',
+    author: 'Edsger W. Dijkstra',
+    url: 'http://www.u.arizona.edu/~rubinson/copyright_violations/Go_To_Considered_Harmful.html',
+    likes: 5,
+    __v: 0
+  }
+]
+const biggerList = [
+    {
         _id: '5a422aa71b54a676234d17f8',
         title: 'Go To Statement Considered Harmful',
         author: 'Edsger W. Dijkstra',
         url: 'http://www.u.arizona.edu/~rubinson/copyright_violations/Go_To_Considered_Harmful.html',
         likes: 5,
         __v: 0
-      }
-    ]
-    const biggerList = [
-        {
-            _id: '5a422aa71b54a676234d17f8',
-            title: 'Go To Statement Considered Harmful',
-            author: 'Edsger W. Dijkstra',
-            url: 'http://www.u.arizona.edu/~rubinson/copyright_violations/Go_To_Considered_Harmful.html',
-            likes: 5,
-            __v: 0
-          },
-          {
-            _id: '5a422aa71b54a676234d17f1',
-            title: 'Testi 2',
-            author: 'Edsger W. Dijkstra',
-            url: 'http://www.u.arizona.edu/~rubinson/copyright_violations/Go_To_Considered_Harmful.html',
-            likes: 5,
-            __v: 0
-          },
-          {
-            _id: '5a422aa71b54a676234d17f2',
-            title: 'Testi 3',
-            author: 'Edsger W. Dijkstra',
-            url: 'http://www.u.arizona.edu/~rubinson/copyright_violations/Go_To_Considered_Harmful.html',
-            likes: 21,
-            __v: 0
-          }       
-    ]
+      },
+      {
+        _id: '5a422aa71b54a676234d17f1',
+        title: 'Testi 2',
+        author: 'Edsger W. Dijkstra',
+        url: 'http://www.u.arizona.edu/~rubinson/copyright_violations/Go_To_Considered_Harmful.html',
+        likes: 5,
+        __v: 0
+      },
+      {
+        _id: '5a422aa71b54a676234d17f2',
+        title: 'Testi 3',
+        author: 'Edsger W. Dijkstra',
+        url: 'http://www.u.arizona.edu/~rubinson/copyright_violations/Go_To_Considered_Harmful.html',
+        likes: 21,
+        __v: 0
+      }       
+]
+
+const evenBiggerList = [
+  {
+      _id: '5a422aa71b54a676234d17f8',
+      title: 'Go To Statement Considered Harmful',
+      author: 'Edsger W. Dijkstra',
+      url: 'http://www.u.arizona.edu/~rubinson/copyright_violations/Go_To_Considered_Harmful.html',
+      likes: 5,
+      __v: 0
+    },
+    {
+      _id: '5a422aa71b54a676234d17f1',
+      title: 'Testi 2',
+      author: 'Edsger W. Dijkstra',
+      url: 'http://www.u.arizona.edu/~rubinson/copyright_violations/Go_To_Considered_Harmful.html',
+      likes: 5,
+      __v: 0
+    },
+    {
+      _id: '5a422aa71b54a676234d17f2',
+      title: 'Testi 3',
+      author: 'Edsger W. Dijkstra',
+      url: 'http://www.u.arizona.edu/~rubinson/copyright_violations/Go_To_Considered_Harmful.html',
+      likes: 21,
+      __v: 0
+    },
+    {
+      _id: '5a422aa71b54a676234d17v3',
+      title: 'testi 1',
+      author: 'Testi testinen',
+      url: 'http://www.u.arizona.edu/~rubinson/copyright_violations/Go_To_Considered_Harmful.html',
+      likes: 5,
+      __v: 0
+    },
+    {
+      _id: '5a422aa71b54a676234d17s2',
+      title: 'Testi 5',
+      author: 'Joku vaan',
+      url: 'http://www.u.arizona.edu/~rubinson/copyright_violations/Go_To_Considered_Harmful.html',
+      likes: 5,
+      __v: 0
+    },
+    {
+      _id: '5a422aa71b54a676234d17r1',
+      title: 'Testi 6',
+      author: 'MAtti meikäläinen',
+      url: 'http://www.u.arizona.edu/~rubinson/copyright_violations/Go_To_Considered_Harmful.html',
+      likes: 21,
+      __v: 0
+    }       
+]
+
+describe('total likes', () => {
+    
     test('of empty list is zero', () => {
         const result = listHelper.totalLikes(emptyList)
         assert.strictEqual(result, 0)
@@ -61,7 +113,22 @@ describe('total likes', () => {
         const result = listHelper.totalLikes(biggerList)
         assert.strictEqual(result, 31)
       })
-  })
+    })
+describe('favorite', () => {
+    test('the favorite blog with most likes', () => {
+        const result = listHelper.favoriteBlog(evenBiggerList)
+        assert.strictEqual(result, evenBiggerList[2])
+      })
+  
+    test('the author with most blogs', () => {
+      const result = listHelper.mostBlogs(evenBiggerList)
+      assert.strictEqual(result, 'Edsger W. Dijkstra')
+    })
+    test('the author with most likes', () => {
+      const result = listHelper.mostLikes(evenBiggerList)
+      assert.deepStrictEqual(result, { author: "Edsger W. Dijkstra", likes: 31 })
+    })
+})
 
 
 /*
